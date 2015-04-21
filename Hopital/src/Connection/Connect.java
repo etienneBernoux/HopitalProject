9385a9558a3d7fa89d2601b22bb26df64d37d907
Etienne <Etienne@Etienne-PC>
2015-04-21 20:07:19 +0200

    Connection fonctionnel a ma bbd, chargement et affichage des chambres

--- Hopital/src/Connection/Connect.java
+++ Hopital/src/Connection/Connect.java
@@ -22,31 +22,63 @@ public class Connect {
     private static Connection conn;
     private static Statement stmt;
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
 
-        // chargement driver "com.mysql.jdbc.Driver"
-        Class.forName("com.mysql.jdbc.Driver");
-        System.out.println("Driver Ok !");
+        try {
 
-        // Connexion via le tunnel SSH avec le username et le password ECE
-        SSHTunnel ssh = new SSHTunnel("ebernoux", "J@hab16AR");
+            // chargement driver "com.mysql.jdbc.Driver"
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Ok !");
 
-        if (ssh.connect()) {
-            System.out.println("Connexion reussie");
-            // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
-            String urlDatabase = "jdbc:mysql://localhost:3305/" + "ebernoux";
-            String user = "ebernoux-rw";
-            String passwd = "erWVgk63";
+            // Connexion via le tunnel SSH avec le username et le password ECE
+            SSHTunnel ssh = new SSHTunnel("ebernoux", "J@hab16AR");
 
-            
+            if (ssh.connect()) {
+                System.out.println("Connexion reussie");
+                // url de connexion "jdbc:mysql://localhost:3305/usernameECE"
+                String urlDatabase = "jdbc:mysql://localhost:3305/" + "ebernoux";
+                String user = "ebernoux-rw";
+                String passwd = "erWVgk63";
 
-            //création d'une connexion JDBC à la base
-            conn = DriverManager.getConnection(urlDatabase,user,passwd);
+                //création d'une connexion JDBC à la base
+                conn = DriverManager.getConnection(urlDatabase, user, passwd);
+                
+                System.out.println("Connexion fonctionel !");
 
-            // création d'un ordre SQL (statement)
-            stmt = conn.createStatement();
-            
-            System.out.println("Connexion effective !");
+                // création d'un ordre SQL (statement)
+                stmt = conn.createStatement();
+
+                
+
+                //L'objet ResultSet contient le résultat de la requête SQL
+                ResultSet result = stmt.executeQuery("SELECT * FROM chambre");
+                //On récupère les MetaData
+                ResultSetMetaData resultMeta = result.getMetaData();
+
+                System.out.println("\n**********************************");
+                //On affiche le nom des colonnes
+                for (int i = 1; i <= resultMeta.getColumnCount(); i++) {
+                    System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+                }
+
+                System.out.println("\n**********************************");
+
+                while (result.next()) {
+                    for (int i = 1; i <= resultMeta.getColumnCount(); i++) {
+                        System.out.print("\t" + result.getObject(i).toString() + "\t |");
+                    }
+
+                    System.out.println("\n---------------------------------");
+
+                }
+
+                result.close();
+                stmt.close();
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("erreur de connection");
         }
 
     }
