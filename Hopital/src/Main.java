be7e350630e61243f8c42c366e81897c54781774
Etienne <Etienne@Etienne-PC>
2015-04-25 18:40:46 +0200

    Création d'une classe de connection

--- Hopital/src/Main.java
+++ Hopital/src/Main.java
@@ -1,3 +1,7 @@
+
+import Connection.ConnectionEce;
+import java.sql.SQLException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,8 +17,17 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        //On lance la connection
+        ConnectionEce connection = new ConnectionEce("ebernoux","J@hab16AR"," ebernoux-rw","erWVgk63");
+        //connection.Selectiontest("SELECT code_service FROM chambre WHERE code_service='REA'");
+        connection.affichageresultconsole();
+        //On ferme la connection
+        connection.fermerConnection();
+        System.exit(0);
+        
+        
     }
     
 }
